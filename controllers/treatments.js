149260a2c37be053f79a09485268b5a005f8c2b6
Rami <frami.contact@gmail.com>
2021-06-21 23:33:16 +0200

    :recycle:: refacto treatments route and controller

--- controllers/treatments.js
+++ controllers/treatments.js
@@ -38,17 +38,83 @@ exports.treatments_get_all_By_Animal = async (req, res) => {
         return res.status(400).json({ 'error': 'Wrong Token' });
     } 
 
-    let pMedicamentId = req.params.idAnimal;
+    try {
+        const treatments = await models.Event.findAll({
+            where: { animalId: req.params.idAnimal }
+        });
+        return res.status(200).json(treatments);
+    } catch (e) {
+        return res.status(400).json({ 'status': 400, message: e.message });
+    }
+}
+
+exports.treatments_get_treatment = async (req, res) => {
+    
+    let headerAuth = req.headers['authorization'];
+    let userId = jwtUtils.getUserId(headerAuth);
+
+    if (userId < 0) {
+        return res.status(400).json({ 'error': 'Wrong Token' });
+    } 
+
+    try {
+        const treatment = await models.Event.findOne({
+            where: { id: req.params.idTreatment }
+        });
+        return res.status(200).json(treatment);
+    } catch (e) {
+        return res.status(400).json({ 'status': 400, message: e.message });
+    }
+}
+
+exports.treatments_update_treatment = async (req, res) => {
+
+    // TODO : change attributs
+    let idTreatment = req.params.idTreatment;
+    let pMedicamentId = req.body.medicamentId;
+    let pDatHeureRappel = req.body.dateHeureRappel;
+    let pStatut = req.body.statut;
 
     models.Event.findOne({
-        where: { id: pMedicamentId }
+        where: { id: idTreatment }
     }).then(function (eventFound) {
         if (eventFound) {
-            return res.status(200).json(eventFound);
+             eventFound.update({
+                medicamentId: (pMedicamentId ? pMedicamentId : eventFound.medicamentId),
+                dateHeureRappel: (pDatHeureRappel ? pDatHeureRappel : eventFound.dateHeureRappel),
+                statut: (pStatut ? pStatut : eventFound.statut)
+             }).then(function (eventUpdated) {
+                return res.status(200).json(eventUpdated);
+             }).catch(function (error) {
+                return res.status(500).json({ 'Error ': error + ' Mis à jour de l\'évènement impossible.' });
+             });
         } else {
-            return res.status(400).json({ 'Error ' : 'L\'évènement demandé n\'est pas dans la base de données.' });
+            return res.status(400).json({ 'Error ': ' L\'évènement n\'est pas dans la base de données.' });
         }
     }).catch(function (error) {
-        return res.status(500).json({ 'Error ' : error + ' Impossible de récupérer l\'évènement demandé' });
+        return res.status(500).json({ 'Error ': error + ' Recherche de l\'évènement impossible.' });
     });
 }
+
+exports.treatments_delete_treatment = async  (req, res) => {
+
+    let idTreatment = req.params.idTreatment;
+
+    models.Event.findOne({
+        where: { id: idTreatment }
+    }).then(function (eventFound) {
+        if (eventFound) {
+             eventFound.destroy({
+             }).then(function (eventDeletd) {
+                return res.status(200).json(eventDeletd);
+             }).catch(function (error) {
+                return res.status(500).json({ 'Error ': error + ' Suppression de l\'évènement impossible.' });
+             })
+        } else {
+            return res.status(404).json({ 'Error ': ' L\'évènement n\'est pas dans la base de données.' });
+        }
+    }).catch(function (error) {
+        return res.status(500).json({ 'Error ': error + ' Recherche de l\'évènement impossible.' });
+    });
+}
+
--- routes/treatments.js
+++ routes/treatments.js
@@ -1,65 +1,18 @@
 // Imports
 const express = require('express');
 const router = express.Router();
-const models = require('../models');
 const treatmentsController = require('../controllers/treatments');
 
 
 router.post('/new', treatmentsController.treatments_create);
 
-router.get('/:idAnimal', );
-router.get('/:idTreatment', );
+router.get('/:idAnimal', treatmentsController.treatments_get_all_By_Animal);
 
-router.patch('/:idEvent', (req, res) => {
-    // Param
-    var pIdEvent = req.params.idEvent;
-    var pMedicamentId = req.body.medicamentId;
-    var pDatHeureRappel = req.body.dateHeureRappel;
-    var pStatut = req.body.statut;
+router.get('/:idTreatment', treatmentsController.treatments_get_treatment);
 
-    models.Event.findOne({
-        where: { id: pIdEvent }
-    }).then(function (eventFound) {
-        if (eventFound) {
-             eventFound.update({
-                medicamentId: (pMedicamentId ? pMedicamentId : eventFound.medicamentId),
-                dateHeureRappel: (pDatHeureRappel ? pDatHeureRappel : eventFound.dateHeureRappel),
-                statut: (pStatut ? pStatut : eventFound.statut)
-             }).then(function (eventUpdated) {
-                return res.status(200).json(eventUpdated);
-             }).catch(function (error) {
-                return res.status(500).json({ 'Error ': error + ' Mis à jour de l\'évènement impossible.' });
-             });
-        } else {
-            return res.status(400).json({ 'Error ': ' L\'évènement n\'est pas dans la base de données.' });
-        }
-    }).catch(function (error) {
-        return res.status(500).json({ 'Error ': error + ' Recherche de l\'évènement impossible.' });
-    });
-});
+router.patch('/:idTreatment', treatmentsController.treatments_update_treatment);
 
-router.delete('/:idEvent', (req, res) => {
-    // Param
-    var pIdEvent = req.params.idEvent;
-
-    models.Event.findOne({
-        where: { id: pIdEvent }
-    }).then(function (eventFound) {
-        if (eventFound) {
-             eventFound.destroy({
-             }).then(function (eventDeletd) {
-                return res.status(200).json(eventDeletd);
-             }).catch(function (error) {
-                return res.status(500).json({ 'Error ': error + ' Suppression de l\'évènement impossible.' });
-             })
-        } else {
-            return res.status(404).json({ 'Error ': ' L\'évènement n\'est pas dans la base de données.' });
-        }
-    }).catch(function (error) {
-        return res.status(500).json({ 'Error ': error + ' Recherche de l\'évènement impossible.' });
-    });
-    
-});
+router.delete('/:idTreatment', treatmentsController.treatments_delete_treatment);
 
 
 module.exports = router;
